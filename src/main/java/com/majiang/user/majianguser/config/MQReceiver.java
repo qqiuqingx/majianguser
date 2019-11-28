038a7bc28816474f50302458e161ac4f9c2063e6
qqiuqingx <qqiuqingx@163.com>
2019-11-28 18:06:25 +0800

    11.28，待修改从mq中拿数据的逻辑

--- src/main/java/com/majiang/user/majianguser/config/MQReceiver.java
+++ src/main/java/com/majiang/user/majianguser/config/MQReceiver.java
@@ -1,17 +1,20 @@
 package com.majiang.user.majianguser.config;
 
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class MQReceiver {
     @Value("${majiang.mq.mqQueueName}")
     public String mqQueueName;
-
-    @RabbitListener(queues = mqQueueName)
+    public static final String Name="majiangQueue";
+    @RabbitHandler
+    @RabbitListener(queues = Name)
     public void majiangorder(String massage){
-
+        System.out.println("mq队列里获取到的数据:"+massage);
+        System.out.println("mq:"+mqQueueName);
+        System.out.println("mq:"+mqQueueName.getClass());
     }
 }
