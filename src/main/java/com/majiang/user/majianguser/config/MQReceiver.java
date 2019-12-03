055924f0e50e13aea1dba774690a997f115150e3
qqiuqingx <qqiuqingx@163.com>
2019-12-03 19:36:10 +0800

    12.03 队列手动应答

--- src/main/java/com/majiang/user/majianguser/config/MQReceiver.java
+++ src/main/java/com/majiang/user/majianguser/config/MQReceiver.java
@@ -8,9 +8,11 @@ import com.majiang.user.majianguser.service.MajiangService;
 import com.majiang.user.majianguser.service.impl.UserInfoserviceImpl;
 import com.majiang.user.majianguser.utils.DesUtil;
 import com.majiang.user.majianguser.utils.RedisUtils;
+import com.rabbitmq.client.Channel;
 import io.lettuce.core.ScriptOutputType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
@@ -33,8 +36,8 @@ public class MQReceiver {
     MajiangService majiangService;
     @RabbitHandler
     @RabbitListener(queues = MyMqConfig.QUEUE_NAME)
-    @Transactional  //为什么在这里加事务注解，只能入一条相同的数据
-    public void majiangorder(String massage){
+   // @Transactional  //为什么在这里加事务注解，只能入一条相同的数据
+    public void majiangorder(String massage, Channel channel, Message messages) throws IOException {
         MajiangUserBean majiangUserBean = new Gson().fromJson(massage, MajiangUserBean.class);
         String userPhone = majiangUserBean.getUserPhone();
         System.out.println("majianguser:"+majiangUserBean);
@@ -45,7 +48,7 @@ public class MQReceiver {
                 return;
             }
             //更新麻将桌数
-            redisUtils.set(String.valueOf(majiangUserBean.getMajiangKeyID()), majiang.getNum());
+           // redisUtils.set(String.valueOf(majiangUserBean.getMajiangKeyID()), majiang.getNum());
             majiangUserBean.setUserPhone(DesUtil.encode(DesUtil.KEY, majiangUserBean.getUserPhone()));
             //根据用户手机和麻将KeyID来获取订单
             List<MajiangUserBean> muByKeyIDandUserPhone = majiangService.getMUByKeyIDandUserPhone(String.valueOf(majiangUserBean.getMajiangKeyID()), majiangUserBean.getUserPhone());
@@ -62,13 +65,19 @@ public class MQReceiver {
             }
             // TODO 更新麻将表中的桌数
             Integer newNum = (Integer) redisUtils.get(String.valueOf(majiangUserBean.getMajiangKeyID()));
+            System.out.println("redis中的num:"+newNum);
             majiang.setNum(newNum);
             majiangService.updateMajiang(majiang);
             // TODO 更新redis中的数据
             // TODO 返回？
+
+            //手动确认消息，需要先配置:acknowledge-mode: manual
+            channel.basicAck(messages.getMessageProperties().getDeliveryTag(),false);
         }catch (Exception e){
             LOGGER.error("消费者错误:",e);
            // TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            //未收到消息
+            channel.basicNack(messages.getMessageProperties().getDeliveryTag(),false,false);
         }finally {
             System.out.println("mq队列里获取到的数据:"+massage);
         }
