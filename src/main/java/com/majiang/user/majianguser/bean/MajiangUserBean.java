d642312561dacda1deddb5d7ea4d030b8a8bbceb
qqiuqingx <qqiuqingx@163.com>
2019-12-31 17:31:15 +0800

    12.31

--- src/main/java/com/majiang/user/majianguser/bean/MajiangUserBean.java
+++ src/main/java/com/majiang/user/majianguser/bean/MajiangUserBean.java
@@ -1,9 +1,13 @@
 package com.majiang.user.majianguser.bean;
 
+import com.majiang.user.majianguser.enums.MajiangUserOrderEnum;
+
 public class MajiangUserBean  extends  PrentBean {
     private  Integer keyID;
     private String userPhone;
     private Integer majiangKeyID;
+    private Integer Status;
+    private String statusName;
 
     @Override
     public String toString() {
@@ -11,7 +15,33 @@ public class MajiangUserBean  extends  PrentBean {
                 "keyID=" + keyID +
                 ", userPhone='" + userPhone + '\'' +
                 ", majiangKeyID=" + majiangKeyID +
+                ", Status=" + Status +
+                ", statusName='" + statusName + '\'' +
                 "} " + super.toString();
+    }
+
+    public MajiangUserBean setStatusandName(MajiangUserOrderEnum majiangUserOrderEnum){
+        this.Status=majiangUserOrderEnum.getOrderStatus();
+        this.statusName=majiangUserOrderEnum.getOrderStatusName();
+        return this;
+    }
+
+    public Integer getStatus() {
+        return Status;
+    }
+
+    public MajiangUserBean setStatus(Integer status) {
+        Status = status;
+        return this;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public MajiangUserBean setStatusName(String statusName) {
+        this.statusName = statusName;
+        return this;
     }
 
     public Integer getKeyID() {
